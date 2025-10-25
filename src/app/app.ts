fa9ce00fe92a99a3312016375912a65c620fee60
celsobritodev <celsinhobrito@gmail.com>
2025-10-25 09:48:43 -0300

    feat: create initial structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DefaultLoginLayout } from "./components/default-login-layout/default-login-layout";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, DefaultLoginLayout],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
