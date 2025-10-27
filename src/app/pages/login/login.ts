39771ce16478fafc3aa5cb038278675997f9f07c
celsobritodev <celsinhobrito@gmail.com>
2025-10-27 09:55:39 -0300

    criar logica de comunicao login front x back

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,26 +1,36 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayout } from '../../components/default-login-layout/default-login-layout';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { PrimaryInput } from "../../components/primary-input/primary-input";
+import { PrimaryInput } from '../../components/primary-input/primary-input';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login-service';
 
 @Component({
   selector: 'app-login',
   imports: [DefaultLoginLayout, ReactiveFormsModule, PrimaryInput],
+  providers: [LoginService],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-
 export class Login {
-
   loginForm!: FormGroup;
 
-  constructor() {
+  constructor(private router: Router, private loginService: LoginService) {
     this.loginForm = new FormGroup({
-      email: new FormControl('',[Validators.required,Validators.email]),
-      password: new FormControl('',[Validators.required,Validators.minLength(6)])
-    })
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
   }
 
+  submit() {
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next:() => console.log("sucesso"),
+      error:() => console.log("error")
+    })
 
+  }
 
+  navigate() {
+    this.router.navigate(['signup']);
+  }
 }
